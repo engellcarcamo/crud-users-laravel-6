d45dde7fc152771249a91ee3e379bc008472d211
Engels Càrcamo <esnaydercarcamo59@gmail.com>
2024-03-16 21:40:14 -0600

    Add example routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,26 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users', function () {
+    return 'User list';
+});
+Route::get('users/{id}', function ($id) {
+    return "showing user detail: $id";
+})->where('id', '[0-9]+');
+
+Route::get('/users/new', function () {
+    return ('Create a new user');
+});
+
+Route::get('/users/{id}/edit', function ($id) {
+    return "Edit the user with id: $id";
+})->where('id', '[0-9]+');
+
+Route::get('/greeting/{name}/{nickname?}', function ($name, $nickname = null) {
+    $name = ucfirst($name);
+    if (!$nickname) {
+        return "Welcome $name";
+    }
+    return "Welcome $name $nickname";
+});
